01a26d1ae98e507b860911d0955b0feee26b0d3b
Chengeng Liu <c.liu75@student.unimelb.edu.au>
2019-09-14 12:01:22 +1000

    Update. Format the ouput data

--- use-api.py
+++ use-api.py
@@ -7,7 +7,9 @@ class CommonWealth:
         self.host_url = host_url
         self.headers_ = headers_
     def comwealth_products(self):
+        # Retrieve JSON from Com Bank
         r = requests.get(url=self.host_url, headers=self.headers_)
+        # print(r.text)
         return r.text
     def output_file(self,json_file):
         with open('comwealth_origin_products.json', 'w') as f:
@@ -24,66 +26,83 @@ class CommonWealth:
                     print(json_file["data"]["products"][i]["name"])
             csv_writer.close()
 
+    # Retrieve details about one product
     def fetch_product(self, product_id):
         r = requests.get(url=self.host_url+"/"+product_id, headers=self.headers_)
         return  r.text
 
 
-    def format_product_details(self,product_content):
-        with open('comwealth_prodcuts_indetail.txt', 'w') as f:
+    def format_product_details(self,product_content, product_name):
+        with open ('comwealth_products_indetail.txt', 'a') as f:
+            f.write(product_name)
+            f.write("\n")
+            my_print_dict(product_content, f)
+            f.write("\n")
+        f.close()
+        print("ok")
 
 
+    # Fetch the products details (all products)
+    # Receive the argument that is a python object.
     def fetch_product_all_details(self, json_file):
         for i in range(0,json_file["meta"]["totalRecords"]-1):
-            product_json_content =
+            product = self.fetch_product(json_file["data"]["products"][i]["productId"])   # one product
+            dict_product = json.loads(product)
+            self.format_product_details(dict_product, dict_product["data"]["name"])
+            # self.format_test(product)
+
+            #Format the data
+            # self.format_product_details(product, json.loads(product)["data"]["name"])
+
+    def format_test(self):
+        full_content = self.comwealth_products()
+        content_id= (json.loads(full_content))["data"]["products"][0]["productId"]
+        # print(content["data"]["products"][0]["productId"])
+        print(content_id)
+        one_product = self.fetch_product(content_id)  # Return a json format string.
+        print(one_product)
+        dic_json = json.loads(one_product)
+        with open ('test.txt', 'w') as f:
+            my_print_dict(dic_json, f)
+            f.write("\n")
+        f.close()
+        print("ok")
 
 
 
+'''
+This function receieves a dictionary and formats its output by recursion. 
+However, this function will not be able to deal with list in dictionary. 
+d: dictionary that needs to iterate
+f: the file-writer
+'''
+def my_print_dict(d, f):
+    for k, v in d.items():
+        if isinstance(v, dict):
+            my_print_dict(v,f)
+            f.write("\n")
+        else:
+            f.write("{0}: {1} \n".format(k,v))
 
 
 
-      
 
 def main():
     comwealth_host_url = "https://api.commbank.com.au/cds-au/v1/banking/products"
     comwealth_headers = {'Accept': "application/json"}
     comwealth = CommonWealth(comwealth_host_url, comwealth_headers)
-    content_json = json.loads(comwealth.comwealth_products())  # Content returned as unicode.
-    # print(comwealth.comwealth_products())
-    # print(content_json)
-
-
+    content_json = json.loads(comwealth.comwealth_products())  # Content returned as unicode and loaded as a python object.
 
     # Store original products json 
     comwealth.output_file(content_json)
-    # Format the json and output json
+    # Format the json and output json. No use once finised.
     # comwealth.format_data(content_json)
 
-    first_product = json.loads(comwealth.fetch_product(content_json["data"]["products"][0]["productId"]))
-    with open ('first_product.json', 'w') as f:
-        json.dump(first_product, f)
-    f.close()
+    comwealth.fetch_product_all_details(content_json)
 
-    # print(content_json["data"]["products"][0]["productId"])
-
-    # for i in range(0,content_json["meta"]["totalRecords"]-1):
-    #     print(content_json["data"]["products"][i]["name"], i)
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-# content_json = json.loads(comwealth_products())
-# output_file(content_json)
-
-
-
-# print(json.dumps(comwealth_products().decode("utf-8"), sort_keys=True, indent=4, separators=(',', ': ')))
-# with open('data2.json','w') as f:
-#     f.write(json.dumps(comwealth_products().decode("utf-8"), sort_keys=True, indent=4, separators=(',', ': ')))
 
 
 
